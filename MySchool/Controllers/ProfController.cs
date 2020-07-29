e87d7745371cb7ee7fb020a301764cb06165e265
Eel2000 <eliezerbwana@gmaill.com>
2020-07-29 11:56:29 +0200

    adding action for new enseignant and retrieving all those enseignant on our db and display them

--- MySchool/Controllers/ProfController.cs
+++ MySchool/Controllers/ProfController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MySchool.Data;
+using MySchool.Models;
 
 namespace MySchool.Controllers
 {
@@ -11,14 +14,58 @@ namespace MySchool.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
 
+        public SelectList OptionNameSL { get; set; }
+
+
+
         public ProfController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
+        public void PopulateOptionsDropDownList(object selectedOption = null)
+        {
+            var OptionsQuery = from c in _dbContext.Options
+                               orderby c.Designation //sort by designation
+                               select c;
+
+            ViewBag.OptionNameSL = new SelectList(OptionsQuery.AsNoTracking(), "OptionID", "Designation", selectedOption);
+
+        }
+
         public IActionResult Index()
         {
+            PopulateOptionsDropDownList();
             return View();
+        }
+
+        [HttpPost,ActionName("Add")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddProf([Bind("FirstName,LastName,Email,PhoneNumber,OptionID")] Enseignant enseignant)
+        {
+            if (ModelState.IsValid)
+            {
+                var newProf = enseignant;
+
+                await _dbContext.Enseignants.AddAsync(newProf);
+                await _dbContext.SaveChangesAsync();
+
+                await Task.Delay(2000);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListOfProf()
+        {
+            var profDisponible = await _dbContext.Enseignants
+                .Include(p => p.Option)
+                    .ThenInclude(p => p.Cours)
+                        .ToListAsync();
+
+            return View(profDisponible);
         }
     }
 }
